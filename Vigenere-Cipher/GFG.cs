fca401409b8907752c6ccc71129479792ae3717a
Daniel Victor <daniel.silva@apinteractive.com.br>
2020-10-27 10:24:03 -0300

    Implementando código Vigerene

---
+++ Vigenere-Cipher/GFG.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vigenere_Cipher
+{
+	// C# code to implement Vigenere Cipher 
+	public class GFG
+	{
+
+		// This function generates the key in 
+		// a cyclic manner until it's length isi'nt 
+		// equal to the length of original text 
+		public static String generateKey(String str, String key)
+		{
+			int x = str.Length;
+
+			for (int i = 0; ; i++)
+			{
+				if (x == i)
+					i = 0;
+				if (key.Length == str.Length)
+					break;
+				key += (key[i]);
+			}
+			return key;
+		}
+
+		// Essa função deve retornar o texto encriptado
+		// gerado com a ajuda do key
+		public static String cipherText(String str, String key)
+		{
+			String cipher_text = "";
+
+			for (int i = 0; i < str.Length; i++)
+			{
+				
+				int x = (str[i] + key[i]) % 26;
+
+
+				x += 'A';
+
+				cipher_text += (char)(x);
+			}
+			return cipher_text;
+		}
+
+		// This function decrypts the encrypted text 
+		// and returns the original text 
+		public static String originalText(String cipher_text, String key)
+		{
+			String orig_text = "";
+
+			for (int i = 0; i < cipher_text.Length &&
+									i < key.Length; i++)
+			{
+				// converting in range 0-25 
+				int x = (cipher_text[i] -
+							key[i] + 26) % 26;
+
+				// convert into alphabets(ASCII) 
+				x += 'A';
+				orig_text += (char)(x);
+			}
+			return orig_text;
+		}
+	}
+}
+/* This code contributed by PrinciRaj1992 */
---
+++ Vigenere-Cipher/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Xml.Schema;
+
+namespace Vigenere_Cipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-=-=- Vigere Cipher -=-=-");
+            Console.Write("Insira o texto simples: ");
+            string str = Console.ReadLine();
+            Console.Write("Insira a palavra-chave: ");
+            string keyword = Console.ReadLine();
+
+            String key = GFG.generateKey(str.ToUpper(), keyword.ToUpper());
+            String cipher_text = GFG.cipherText(str.ToUpper(), key.ToUpper());
+
+            Console.WriteLine("Ciphertext : "
+                + cipher_text + "\n");
+
+
+            Console.WriteLine("Original/Decrypted Text : "
+                + GFG.originalText(cipher_text, key));
+        }
+    }
+}
